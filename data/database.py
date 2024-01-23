e3179dd09cbe4795e8f2db42c3e7fd41d7561a9b
henry <wspoutsource@pixelfield.cz>
2024-01-23 11:16:42 +0700

    change db host

--- data/database.py
+++ data/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Hung0112256@mysql:3306/db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Hung0112256@0.tcp.ap.ngrok.io:18889/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
